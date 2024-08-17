d01c3d5ad23133b5ba0a956e5cd1bd48501293ab
Doge.Dev <110031714+dogedev1@users.noreply.github.com>
2024-08-17 23:17:15 +1000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Doge Development'
+description 'Get logs when member's spawn cars in'
+version '1.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
